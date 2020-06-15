3f0a30f584401e65064d98dfd6a53652b99277fa
ago <ago@localhost.localdomain>
2020-06-15 15:17:46 +0300

    love app2

--- clock.py
+++ clock.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-from love import send_love
+from love import send_love, send_love_captain
 
 sched = BlockingScheduler()
 
-# Schedule job_function to be called every 5 sec
-sched.add_job(send_love, 'interval', seconds=5)
+# Schedule job_function to be called every 60 sec
+sched.add_job(send_love, 'interval', seconds=60)
+# Schedule job_function to be called every 60 sec
+sched.add_job(send_love_captain, 'interval', seconds=60)
 
 sched.start()

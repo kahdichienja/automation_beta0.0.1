afd47538529fcb9b36da76e979117c3b72103179
ago <ago@localhost.localdomain>
2020-06-17 11:07:59 +0300

    Added 1 hour interval4

--- love.py
+++ love.py
@@ -6,28 +6,27 @@ client = Client(account_sid, auth_token)
  
 def send_love():
     message = client.messages.create( 
-                              from_='whatsapp:+14155238886',  
-                              body='hey Captain I just automated this message \n \
-                                It run with twilio API \n \
-                                it will be sending msg to you after every 1 hour.\n \
-                                just ignore \n \
-                                if you get it send a screenshot.\
-                                happy hack its CLINTON THE CHIENJAH....  😍😍😍 \n \
-                                I think we can build somthing cool with this API.....are you in',       
-                              to='whatsapp:+254797324006' 
+      from_='whatsapp:+14155238886',
+      body='hey Captain I just automated this message \n \
+        It run with twilio API \n \
+          it will be sending msg to you after every 1 hour.\n \
+            just ignore \n \
+              if you get it send a screenshot.\
+                happy hack its CLINTON THE CHIENJAH....  😍😍😍 \n \
+                  I think we can build somthing cool with this API.....are you in',
+                  to='whatsapp:+254797324006' 
                           )
 def send_love_captain():
-    message = client.messages.create( 
-                              from_='whatsapp:+14155238886',  
-                              body='hey Captain I just automated this message \n \
-                                It run with twilio API \n \
-                                it will be sending msg to you after every 1 hour.\n \
-                                just ignore \n \
-                                if you get it send a screenshot.\
-                                happy hack its CLINTON THE CHIENJAH....  😍😍😍 \n \
-                                I think we can build somthing cool with this API.....are you in',      
-                              to='whatsapp:+254704209395' 
+  message = client.messages.create( 
+      from_='whatsapp:+14155238886',
+      body='hey Captain I just automated this message \n \
+        It run with twilio API \n \
+          it will be sending msg to you after every 1 hour.\n \
+            just ignore \n \
+              if you get it send a screenshot.\
+                happy hack its CLINTON THE CHIENJAH....  😍😍😍 \n \
+                  I think we can build somthing cool with this API.....are you in',
                           )
 
  
-    print(message.sid)
+  print(message.sid)

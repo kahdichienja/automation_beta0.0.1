d3ac587ce280d2620e34bc4b1cb9f8b81463baa1
ago <ago@localhost.localdomain>
2020-06-17 11:24:51 +0300

    Added 1hr

--- clock.py
+++ clock.py
@@ -6,8 +6,8 @@ from love import send_love, send_love_captain
 sched = BlockingScheduler()
 
 # Schedule job_function to be called every 160 sec
-sched.add_job(send_love, 'interval', seconds=60)
+sched.add_job(send_love, 'interval', seconds=3600)
 # Schedule job_function to be called every 160 sec
-sched.add_job(send_love_captain, 'interval', seconds=60)
+sched.add_job(send_love_captain, 'interval', seconds=3600)
 
 sched.start()
